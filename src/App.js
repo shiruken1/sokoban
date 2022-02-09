726cd06998e4db77a0bfd9db0c583df716287bd5
me <amenpa@tutanota.com>
2022-02-09 13:44:22 -0600

    Turns out the boilerplate was using deprecated libs...

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import './styles.css'
-import * as THREE from 'three';
-import React, { useEffect } from "react";
-import { MapControls, Stars } from "drei";
-import { Button, Dropdown } from 'semantic-ui-react'
-import { Canvas, useThree } from "react-three-fiber";
-import { Physics, usePlane, useBox } from 'use-cannon'
-
-// function SpiffyButton({ children }) {
-//   return (
-//     <button className="fancy" onClick={() => console.log('eyyyy')}>
-//       <span className="top-key"></span>
-//       {children}
-//       <span className="bottom-key-1"></span>
-//       <span className="bottom-key-2"></span>
-//     </button>
-//   );
-// }
+// import * as THREE from 'three';
+import React, { useState, useEffect } from 'react';
+import { Canvas, useThree } from '@react-three/fiber';
+import { Button, Menu, Dropdown } from 'semantic-ui-react';
+import { MapControls, Stars, Edges } from '@react-three/drei';
+import { Physics, usePlane, useBox } from '@react-three/cannon';
 
 function Plane(props) {
   const [ ref ] = usePlane(() => ({
@@ -24,46 +13,99 @@ function Plane(props) {
 
   return (
     <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
-    <gridHelper args={[50, 50, 'blue', 'blue']} rotation={[-Math.PI / 2, 0, 0]}/>
+      <gridHelper args={[50, 50, 'blue', 'blue']} rotation={[-Math.PI / 2, 0, 0]}/>
     </mesh>
   );
 }
 
-function Cube(props) {
-  const [ ref ] = useBox(() => ({ mass: 1, position: [0, 1, 0], rotation: [1, 0, 0], ...props }))
+function Cube({ position, isSelected, color }) {
+  const [ ref ] = useBox(() => ({ mass: 1, rotation: [1, 0, 0], position }))
 
   return (
-    <mesh receiveShadow castShadow ref={ref}>
+    <mesh
+      ref={ref}
+      castShadow
+      receiveShadow
+      // onClick={(event) => click(!clicked)}
+      >
       <boxGeometry />
-      <meshLambertMaterial color="hotpink" />
+      <meshLambertMaterial color={color} />
+
+      <Edges visible={isSelected} scale={1.2} >
+        <meshBasicMaterial transparent color="#333" depthTest={false} />
+      </Edges>
+
     </mesh>
   )
 }
 
 export default function App() {
+  const [ primSelected, setPrimSelected ] = useState('');
+
   return (
     <div id="app">
-    <div id="canvas">
       <Canvas
         shadows
         dpr={[1, 2]}
         gl={{ alpha: false }}
         camera={{ position: [0, 10, 0], fov: 45 }}>
-        <MapControls />
-        <Stars />
-        <color attach="background" args={['lightblue']} />
-        <ambientLight />
-        <directionalLight position={[10, 10, 10]} castShadow shadow-mapSize={[2048, 2048]} />
-        <Physics>
-          <Plane position={[-1, -1, -1]} />
-          <Cube position={[0, 0, 0]} />
-        </Physics>
+          <MapControls />
+          <Stars />
+          <color attach="background" args={['lightblue']} />
+          <ambientLight />
+          <directionalLight position={[10, 10, 10]} castShadow shadow-mapSize={[2048, 2048]} />
+          <Physics>
+            <Plane position={[-1, -1, -1]} />
+            <Cube color="hotpink" position={[0, 0, 0]} isSelected={true} />
+          </Physics>
       </Canvas>
-      </div>
+
       <div id="panel">
-        <Button primary onClick={() => console.log('eyyyy')}>Add</Button>
-        <Button onClick={() => console.log('eyyyy')}>Remove</Button>
-        <Button onClick={() => console.log('eyyyy')}>Remove All</Button>
+        <Menu>
+          <Menu.Menu position='left'>
+            <Dropdown
+              item
+              text='Move'
+              upward={true}
+              options={[
+                { key: 1, text: 'North', value: 1 },
+                { key: 2, text: 'East', value: 2 },
+                { key: 3, text: 'South', value: 3 },
+                { key: 4, text: 'West', value: 4 }
+              ]}
+            />
+            <Dropdown
+              item
+              text='Turn'
+              upward={true}
+              options={[
+                { key: 1, text: 'Clockwise', value: 1 },
+                { key: 2, text: 'Counter-Clockwise', value: 2 },
+              ]}
+            />
+          </Menu.Menu>
+          <Menu.Menu position='right'>
+            <Dropdown
+              item
+              text='Add'
+              upward={true}
+              options={[
+                { key: 1, text: 'Red Box', value: 1 },
+                { key: 2, text: 'Green Cylinder', value: 2 },
+                { key: 3, text: 'Blue Pyramid', value: 3 }
+              ]}
+            />
+            <Dropdown
+              item
+              text='Remove'
+              upward={true}
+              options={[
+                { key: 1, text: 'Remove Selected', value: 1 },
+                { key: 2, text: 'Remove All', value: 2 },
+              ]}
+            />
+          </Menu.Menu>
+        </Menu>
       </div>
     </div>
   );
